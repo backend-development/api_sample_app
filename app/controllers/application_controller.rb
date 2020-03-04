8125efde8676455d3a0360451919b0f45c172194
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2020-03-04 16:49:46 +0100

    set request format to json when HTTP_ACCEPT header container application/json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :coerce_json
+
+	def coerce_json
+		# Rails converts the following header:
+		#
+		#	Accept: application/json, text/javascript, */*; q=0.01
+		#
+		# into text/html. Force it back to json.
+		if request.headers[ 'HTTP_ACCEPT' ] =~ /^\s*application\/json/
+			request.format = 'json'
+		end
+	end
 end
