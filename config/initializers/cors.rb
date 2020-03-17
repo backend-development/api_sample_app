1b15843f9b30af0c7f626f174a6262071d8b31dc
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2020-03-17 22:54:51 +0100

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,6 +7,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000', '127.0.0.1:3000', 'https://my-frontend.org'
     resource '/api/v1/*',
-      methods: %i(get post put patch delete options head),
+      methods: %i(get post put patch delete options head)
   end
 end
