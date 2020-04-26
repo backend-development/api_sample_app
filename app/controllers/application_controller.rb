56b56898ef49f309b4d4345cae6d484ccde983a6
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2020-04-26 13:30:51 +0200

    devise aufgesetzt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   # TODO: only verify csrf_token when authentication is done without cookies
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
   skip_before_action :verify_authenticity_token, if: :json_request?
 
   before_action :coerce_json
@@ -13,4 +17,10 @@ class ApplicationController < ActionController::Base
   def json_request?
     request.headers[ 'HTTP_ACCEPT' ] =~ /^\s*application\/json/
   end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, 
+         :recoverable, :rememberable, :validatable
   has_many :credits, class_name: "MoneyTransaction", foreign_key: "creditor_id"
   has_many :debts, class_name: "MoneyTransaction", foreign_key: "debitor_id"  
-  has_secure_password
 
   validates :email, :name, presence: true
   validates :email, :name, uniqueness: true
