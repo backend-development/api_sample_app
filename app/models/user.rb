85315e840295f97249b8e4a5d327fc45ed95abb0
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2020-04-26 14:24:38 +0200

    devise-jwt activated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, 
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :credits, class_name: "MoneyTransaction", foreign_key: "creditor_id"
   has_many :debts, class_name: "MoneyTransaction", foreign_key: "debitor_id"  
 
@@ -13,4 +16,9 @@ class User < ApplicationRecord
   def to_s
     name 
   end
+
+  # additional data for the token
+  def jwt_payload
+    super.merge({ 'foo' => 'bar' })
+  end  
 end
